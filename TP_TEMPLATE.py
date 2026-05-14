32dadfe64933ffa8ba3278b148539a7f2fc04035
Facundo Mangioni <facumangio@gmail.com>
2026-05-14 10:13:26 -0300

    Agregando template de tp y creando unidad 6

---
+++ TP_TEMPLATE.py
@@ -0,0 +1,48 @@
+while True:
+    ejercicio = input(f"Elija un ejercicio del 1 al 10 (0 para cortar): ")
+    match ejercicio:
+        case "1":
+        # Ejercicio 1
+            print("Hola Mundo!")
+
+        # Ejercicio 2
+        case "2":
+
+
+        # Ejercicio 3
+        case "3":
+            
+
+        # Ejercicio 4
+        case "4":
+            
+
+        # Ejercicio 5
+        case "5":
+            
+
+        # Ejercicio 6
+        case "6":
+            
+
+        # Ejercicio 7
+        case "7":
+            
+
+        # Ejercicio 8
+        case "8":
+            
+
+        # Ejercicio 9
+        case "9":
+            
+
+        # Ejercicio 10
+        case "10":
+               
+
+        case "0":
+            print("Terminando programa...")
+            break
+        case _:
+            print("Opcion no valida")
